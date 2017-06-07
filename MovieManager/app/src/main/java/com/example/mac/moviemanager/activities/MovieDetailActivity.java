753d7e59eab7ed33059bc7a12a018e2fd306230a
zhandosbolatbekov <zhabo.fm@gmail.com>
2017-06-07 16:21:02 +0600

    MovieManager done

---
+++ MovieManager/app/src/main/java/com/example/mac/moviemanager/activities/MovieDetailActivity.java
@@ -0,0 +1,53 @@
+package com.example.mac.moviemanager.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.mac.moviemanager.R;
+import com.example.mac.moviemanager.models.Movie;
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MovieDetailActivity extends AppCompatActivity {
+
+    Movie movie = null;
+    @BindView(R.id.ivMovieBackdrop) ImageView ivMovieBackdrop;
+    @BindView(R.id.tvOverview) TextView tvOverview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+        ButterKnife.bind(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Movie saved as favorite", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            movie = (Movie) extras.getSerializable("MOVIE");
+            this.setTitle(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+            Picasso.with(this)
+                    .load(movie.getBackdropPath())
+                    .into(ivMovieBackdrop);
+        }
+    }
+}
---
+++ MovieManager/app/src/main/java/com/example/mac/moviemanager/adapters/MovieRecyclerViewAdapter.java
@@ -0,0 +1,83 @@
+package com.example.mac.moviemanager.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.mac.moviemanager.R;
+import com.example.mac.moviemanager.activities.MovieDetailActivity;
+import com.example.mac.moviemanager.models.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MovieRecyclerViewAdapter extends RecyclerView.Adapter<MovieRecyclerViewAdapter.ViewHolder> {
+
+    private List<Movie> movies;
+    private Context context;
+
+    public Context getContext() {
+        return this.context;
+    }
+
+    public MovieRecyclerViewAdapter(Context context, List<Movie> movies) {
+        this.movies = movies;
+        this.context = context;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        @BindView(R.id.ivMovieImage) ImageView ivMovieImage;
+        @BindView(R.id.tvMovieTitle) TextView tvMovieTitle;
+        @BindView(R.id.tvOverview) TextView tvOverview;
+        @BindView(R.id.cvMovie) CardView cvMovie;
+
+        ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+            Movie movie = movies.get(getAdapterPosition());
+
+            Intent intent = new Intent(getContext(), MovieDetailActivity.class);
+            intent.putExtra("MOVIE", movie);
+            getContext().startActivity(intent);
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_movie, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        Movie curMovie = movies.get(position);
+        holder.tvMovieTitle.setText(curMovie.getTitle());
+        holder.tvOverview.setText(curMovie.getOverview());
+        Picasso.with(getContext())
+                .load(curMovie.getPosterPath())
+                .into(holder.ivMovieImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+}
