b9fba1821b2c5bc7c395cdc9f6d947edb4efb08c
zhandosbolatbekov <zhabo.fm@gmail.com>
2017-06-11 00:50:50 +0600

    added tmdb api for fragments

--- MovieManager/app/src/main/java/com/example/mac/moviemanager/fragments/NowPlayingFragment.java
+++ MovieManager/app/src/main/java/com/example/mac/moviemanager/fragments/NowPlayingFragment.java
@@ -46,7 +46,6 @@ public class NowPlayingFragment extends Fragment {
 
     public NowPlayingFragment() {
         movies = new ArrayList<>();
-
     }
 
     @Override
@@ -55,6 +54,7 @@ public class NowPlayingFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_now_playing, container, false);
         unbinder = ButterKnife.bind(this, view);
+
         LinearLayoutManager llm = new LinearLayoutManager(NowPlayingFragment.this.getContext());
 
         rvMovies.setHasFixedSize(true);
@@ -64,18 +64,12 @@ public class NowPlayingFragment extends Fragment {
 
         new MyAsync().execute();
 
-//        adapter =
-//                new MovieRecyclerViewAdapter(getContext(), movies);
-//
-//        rvMovies.setAdapter(adapter);
-
         return view;
     }
 
     private void initializeData() throws Exception {
 
         OkHttpClient client = new OkHttpClient();
-        String api_key = "041002b871245f52b51405f0f613270e";
         Request request = new Request.Builder()
                 .url("https://api.themoviedb.org/3/movie/now_playing?api_key=041002b871245f52b51405f0f613270e&language=en-US&page=1")
                 .build();
@@ -98,6 +92,7 @@ public class NowPlayingFragment extends Fragment {
             movies.add(m);
 
             Log.d("Json_object", Jobject.toString());
+        }
         /*
         movies.add(new Movie("277834", "Moana", "In Ancient Polynesia, when a terrible curse incurred by Maui reaches an impetuous Chieftain's daughter's island, she answers the Ocean's call to seek out the demigod to set things right.", 6.5f, 854, "/z4x0Bp48ar3Mda8KiPD1vwSY3D8.jpg", "/1qGzqGUd1pa05aqYXGSbLkiBlLB.jpg"));
         movies.add(new Movie("121856", "Passengers", "A spacecraft traveling to a distant colony planet and transporting thousands of people has a malfunction in its sleep chambers. As a result, two passengers are awakened 90 years early.", 6.2f, 745, "/5gJkVIVU7FDp7AfRAbPSvvdbre2.jpg", "/5EW4TR3fWEqpKsWysNcBMtz9Sgp.jpg"));
@@ -105,7 +100,7 @@ public class NowPlayingFragment extends Fragment {
         movies.add(new Movie("283366", "Rogue One: A Star Wars Story", "A rogue band of resistance fighters unite for a mission to steal the Death Star plans and bring a new hope to the galaxy.", 7.2f, 1802, "/qjiskwlV1qQzRCjpV0cL9pEMF9a.jpg", "/tZjVVIYXACV4IIIhXeIM59ytqwS.jpg"));
         movies.add(new Movie("313369", "La La Land", "Mia, an aspiring actress, serves lattes to movie stars in between auditions and Sebastian, a jazz musician, scrapes by playing cocktail party gigs in dingy bars, but as success mounts they are faced with decisions that begin to fray the fragile fabric of their love affair, and the dreams they worked so hard to maintain in each other threaten to rip them apart.", 8, 396, "/ylXCdC106IKiarftHkcacasaAcb.jpg", "/nadTlnTE6DdgmYsN4iWc2a2wiaI.jpg"));
         */
-        }
+
     }
 
     private void dataChanged() {
--- MovieManager/app/src/main/java/com/example/mac/moviemanager/fragments/UpcomingFragment.java
+++ MovieManager/app/src/main/java/com/example/mac/moviemanager/fragments/UpcomingFragment.java
@@ -1,10 +1,12 @@
 package com.example.mac.moviemanager.fragments;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,11 @@ import android.view.ViewGroup;
 import com.example.mac.moviemanager.R;
 import com.example.mac.moviemanager.adapters.MovieRecyclerViewAdapter;
 import com.example.mac.moviemanager.models.Movie;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,9 +36,12 @@ public class UpcomingFragment extends Fragment {
 
     Unbinder unbinder;
 
+    MovieRecyclerViewAdapter adapter;
     private List<Movie> movies;
 
-    public UpcomingFragment() { }
+    public UpcomingFragment() {
+        movies = new ArrayList<>();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,27 +50,71 @@ public class UpcomingFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_upcoming, container, false);
         unbinder = ButterKnife.bind(this, view);
 
-        initializeData();
-
         LinearLayoutManager llm = new LinearLayoutManager(this.getContext());
 
         rvMovies.setHasFixedSize(true);
         rvMovies.setLayoutManager(llm);
 
-        MovieRecyclerViewAdapter adapter =
-                new MovieRecyclerViewAdapter(getContext(), movies);
+        adapter = new MovieRecyclerViewAdapter(getContext(), movies);
         rvMovies.setAdapter(adapter);
+
+        new MyAsync().execute();
 
         return view;
     }
 
-    private void initializeData() {
-        movies = new ArrayList<>();
+    private void initializeData() throws Exception {
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.themoviedb.org/3/movie/upcoming?api_key=041002b871245f52b51405f0f613270e&language=en-US&page=1")
+                .build();
+
+        JSONObject Jobject = new JSONObject(client.newCall(request).execute().body().string().toString());
+        JSONArray Jarray = Jobject.getJSONArray("results");
+        for(int i = 0; i < Jarray.length(); ++i) {
+            JSONObject object = Jarray.getJSONObject(i);
+            String id = object.getString("id");
+            String title = object.getString("title");
+            String overview = object.getString("overview");
+            float voteAverage = Float.valueOf(object.getString("vote_average"));
+            float voteCount = Float.valueOf(object.getString("vote_count"));
+            String posterPath = object.getString("poster_path");
+            String backdropPath = object.getString("backdrop_path");
+            Movie m = new Movie(id, title, overview,
+                    voteAverage, voteCount, posterPath, backdropPath);
+            Log.d("Entity_movie", m.toString());
+            movies.add(m);
+
+            Log.d("Json_object", Jobject.toString());
+        }
+
+        /*
         movies.add(new Movie("277834", "Rings", "Julia (Matilda Lutz) becomes worried about her boyfriend, Holt (Alex Roe) when he explores the dark urban legend of a mysterious videotape said to kill the watcher seven days after viewing. She sacrifices herself to save her boyfriend and in doing so makes a horrifying discovery: there is a \\\"movie within the movie\\\" that no one has ever seen before.", 0f, 0, "/uwNNLJULvTQfgc3PBZAY92EOJQO.jpg", "/91WPDonXsxRzi7AcfedKM3p3NFU.jpg"));
         movies.add(new Movie("121856", "The Great Wall", "The story of an elite force making a last stand for humanity on the world’s most iconic structure.", 0f, 0, "/hm0Z5tpRlSzPO97U5e2Q32Y0Xrb.jpg", "/yESCAoZkaxZ2AMiHojl9jYYd9zD.jpg"));
         movies.add(new Movie("330459", "Patriots Day", "An account of Boston Police Commissioner Ed Davis's actions in the events leading up to the 2013 Boston Marathon bombing and the aftermath, which includes the city-wide manhunt to find the terrorists behind it.", 0f, 0, "/gd4SDPZIdVRAxUolQ9ZCNXTkQUq.jpg", "/tiBL4PeaCPKGBz3qO4dJP2KzKop.jpg"));
         movies.add(new Movie("283366", "T2 Trainspotting", "First there was an opportunity......then there was a betrayal. Twenty years have gone by. Much has changed but just as much remains the same. ", 0f, 0, "/A84pxL5z86KbV9u8GAK2mVOoXK.jpg", "/dxJynNxoG6jRttYPoXOGx7OWJvq.jpg"));
+        */
+    }
 
+    private class MyAsync extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            try {
+                initializeData();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            adapter.notifyDataSetChanged();
+        }
     }
 
     @Override
